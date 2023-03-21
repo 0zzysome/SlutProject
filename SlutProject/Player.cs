adf14afd1eaccd61d5c9fe5eec0f4296b429ae97
0zzysome <oscar.rosin@hotmail.com>
2023-03-21 13:29:07 +0100

    failure

--- SlutProject/Player.cs
+++ SlutProject/Player.cs
@@ -4,17 +4,98 @@ using System;
 
 public class Player : GameObject
 {
-    
+    public float Timer { get; set; }= 1;
+    public int Direction { get; set; }
 
+    //kankse göra till array
+    public Queue<Rectangle> bodyList = new Queue<Rectangle>();
 
-    public Player()
+    public int Movement(int lastMove)
     {
         
 
+        //up 0
+        //> 1
+        //ner 2
+        //< 3
+        int i = lastMove; 
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_UP))
+        {
+            i = 0;
+        }
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT))
+        {
+            i = 1;
+        }
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_DOWN))
+        {
+            i = 2;
+        }        
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT))
+        {
+            i = 3;
+        }
+        return i;
     }
-    public Rectangle CreateBody(int Interval)
+    /*
+
+2
+3
+4
+    */
+    public void remove()
     {
+        Rectangle r = bodyList.Dequeue();
+    }
+    public void addToQueue(int Interval, int direc)
+    {
+        switch (direc)
+        {
+            case 0:
+                Rectangle temp = bodyList.Peek();
+                x = (int)temp.x;
+                y = (int)temp.y - Interval;
+            break;
+            case 1:
+                Rectangle temp1 = bodyList.Peek();
+                x = (int)temp1.x + Interval;
+                y = (int)temp1.y;
+            break;
+            case 2:
+                Rectangle temp2 = bodyList.Peek();
+                x = (int)temp2.x;
+                y = (int)temp2.y + Interval;
+            break;
+            case 3:
+                Rectangle temp3 = bodyList.Peek();
+                x = (int)temp3.x - Interval;
+                y = (int)temp3.y;
+            break;
+            default:
+                Rectangle temp4 = bodyList.Peek();
+                x = (int)temp4.x - Interval;
+                y = (int)temp4.y;
+            break;
+        }
         Rectangle r1 = new Rectangle(x,y, (Interval-1), (Interval-1));
-        return r1;
+        bodyList.Enqueue(r1);
+
+        
+    }
+
+    //startar första boxen
+    public void CreateBody(int Interval)
+    {
+        Rectangle r1 = new Rectangle((Interval*5),(Interval*5), (Interval-1), (Interval-1));
+        bodyList.Enqueue(r1);
+    }
+    public void delayTimer()
+    {
+        
+        if(Timer>0)
+        {
+            Timer-=Raylib.GetFrameTime();
+            return;
+        }
     }   
 }
--- SlutProject/Program.cs
+++ SlutProject/Program.cs
@@ -6,7 +6,7 @@ global using System.Numerics;
 Vector2 screenSize = new(960,960);
 //grid size
 int lineInterval = 40;
-
+int Direction = 0;
 
 
 Raylib.InitWindow((int)screenSize.X,(int)screenSize.Y, "ok");
@@ -14,70 +14,76 @@ Raylib.SetTargetFPS(60);
 
 Player p = new Player();
 apple A = new apple();
+
+
 //reverse queue 
 // lägger till en sist i kön och ta bort första
 // man kontrolerar ifrån senastre delen som lades till 
 
+
+// pre start logik(i construct???)
+Rectangle Apple1 = A.CreateApple(lineInterval);
+
+//skapar första 
+p.CreateBody(lineInterval);
+
 while(!Raylib.WindowShouldClose())
 {
     //logik 
-    Rectangle Apple = null;
-
-    if(A.IsOnBoard == false)
+    if(A.IsOnBoard==false)
     {
-        Apple = A.CreateNewApple((int)screenSize.X/lineInterval, (int)screenSize.Y/lineInterval, lineInterval);
+        Apple1 = A.GiveNewPos((int)screenSize.X/lineInterval, (int)screenSize.Y/lineInterval, lineInterval);
     }
-    Rectangle r1 = p.CreateBody(lineInterval);
-
-    //grafik
-    Raylib.BeginDrawing();
-    Raylib.ClearBackground(Color.WHITE);
-
-    Raylib.DrawRectangleRec(apple, color.red)
-    Raylib.DrawRectangleRec(r1, Color.BLUE);
-
-
-    // draws x net
     
+    p.delayTimer();
+    Direction = p.Movement(Direction);
+    p.addToQueue(lineInterval, Direction);
+
+    
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_F))
+        {
+            A.IsOnBoard = false;
+        }
+    //grafik
+
+    Raylib.BeginDrawing();
+
+    //backgrund
+    Raylib.ClearBackground(Color.WHITE);
+    //x
     for (var i = 0; i < ((int)screenSize.X/lineInterval); i++)
     {
         Raylib.DrawLine(i*lineInterval, 0, i*lineInterval, (int)screenSize.Y, Color.GRAY);   
     }
-    // draws y line
+    //y
     for (var i = 0; i < ((int)screenSize.Y/lineInterval); i++)
     {
         Raylib.DrawLine(0, i*lineInterval, (int)screenSize.Y, i*lineInterval, Color.GRAY);   
     }
 
+    
+    
+    //object
+    Raylib.DrawRectangleRec(Apple1, Color.RED);
+    foreach ( Rectangle r in p.bodyList)
+    {
+        Raylib.DrawRectangleRec(r, Color.BLUE);
+    }
+    
+    
+
    
     
 
     //ritar ut allt 
-    Raylib.DrawRectangleRec(apple, c)
-    Raylib.DrawRectangleRec(r1, Color.BLUE);
+    
 
 
     //movement for now
     // inanför (screenSize-interval)
-    if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT)&& p.x < 920)
-    {
-        p.x += lineInterval;
-    }
-    if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT)&& p.x > 0)
-    {
-        p.x -= lineInterval;
-    }
-    if (Raylib.IsKeyDown(KeyboardKey.KEY_UP)&& p.y > 0)
-    {
-        p.y -= lineInterval;
-    }
-    // inanför (screenSize-interval)
-    if (Raylib.IsKeyDown(KeyboardKey.KEY_DOWN) && p.y < 920)
-    {
-        p.y += lineInterval;
-    }
 
 
+   
 
     Raylib.EndDrawing();
 }
