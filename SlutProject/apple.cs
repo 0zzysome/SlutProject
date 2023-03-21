f14e2c24df3a5c81f195560100b7e39ba6387afc
0zzysome <oscar.rosin@hotmail.com>
2023-03-21 13:29:26 +0100

    fail

--- SlutProject/apple.cs
+++ SlutProject/apple.cs
@@ -6,12 +6,21 @@ public class apple : GameObject
 {
     public bool IsOnBoard { get; set; } = false;
     
-    public Rectangle CreateNewApple(int blockAmountX, int blockAmountY, int Interval)
+
+    //(kan skapas av poperty istället???????????)
+    //prop rectangle med all info och sånt idk
+    
+    public Rectangle CreateApple(int Interval)
+    {
+        // gör rectanglar som är lite mindre än gridet
+        Rectangle r1 = new Rectangle(x,y, (Interval-1), (Interval-1));
+        return r1;
+    }
+    public Rectangle GiveNewPos(int blockAmountX, int blockAmountY, int Interval)
     {
         IsOnBoard = true;
         int x = (generator.Next(blockAmountX)) * Interval;
         int y = (generator.Next(blockAmountY)) * Interval;
-        // gör rectanglar som är lite mindre än gridet
         Rectangle r1 = new Rectangle(x,y, (Interval-1), (Interval-1));
         return r1;
     }
