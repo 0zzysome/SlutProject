02ec70a03e14d5303f3a03482a938a72931ff4b7
0zzysome <oscar.rosin@hotmail.com>
2023-03-22 13:28:22 +0100

    movement and game

--- SlutProject/Program.cs
+++ SlutProject/Program.cs
@@ -10,7 +10,7 @@ int Direction = 0;
 
 
 Raylib.InitWindow((int)screenSize.X,(int)screenSize.Y, "ok");
-Raylib.SetTargetFPS(60);
+Raylib.SetTargetFPS(2);
 
 Player p = new Player();
 apple A = new apple();
@@ -35,11 +35,35 @@ while(!Raylib.WindowShouldClose())
         Apple1 = A.GiveNewPos((int)screenSize.X/lineInterval, (int)screenSize.Y/lineInterval, lineInterval);
     }
     
-    p.delayTimer();
-    Direction = p.Movement(Direction);
-    p.addToQueue(lineInterval, Direction);
+    //nästa mål: skapa en scoreboard
 
-    
+    Direction = p.Movement(Direction);
+    // ser om spelaren har dött (fixa så inget error)
+    if(p.IsDead(screenSize, lineInterval, p.bodyList[p.bodyList.Count-1]))
+    {
+
+        foreach (Rectangle r in p.bodyList)
+        {
+            int 
+        }
+        p.bodyList.Clear();
+
+    }
+    // om inte så flyttar den karatären
+    else
+    {
+        bool AreOverLapping = Raylib.CheckCollisionRecs(Apple1, p.bodyList[p.bodyList.Count-1]);
+        if (AreOverLapping)
+        {
+            p.addToQueue(lineInterval, Direction);    
+            A.IsOnBoard = false;    
+        }
+        else
+        {
+            p.addToQueue(lineInterval, Direction);
+            p.bodyList.RemoveAt(0);         
+        }
+    }
     if (Raylib.IsKeyDown(KeyboardKey.KEY_F))
         {
             A.IsOnBoard = false;
@@ -50,6 +74,7 @@ while(!Raylib.WindowShouldClose())
 
     //backgrund
     Raylib.ClearBackground(Color.WHITE);
+
     //x
     for (var i = 0; i < ((int)screenSize.X/lineInterval); i++)
     {
@@ -60,30 +85,13 @@ while(!Raylib.WindowShouldClose())
     {
         Raylib.DrawLine(0, i*lineInterval, (int)screenSize.Y, i*lineInterval, Color.GRAY);   
     }
-
-    
-    
     //object
     Raylib.DrawRectangleRec(Apple1, Color.RED);
     foreach ( Rectangle r in p.bodyList)
     {
         Raylib.DrawRectangleRec(r, Color.BLUE);
     }
+
     
-    
-
-   
-    
-
-    //ritar ut allt 
-    
-
-
-    //movement for now
-    // inanför (screenSize-interval)
-
-
-   
-
     Raylib.EndDrawing();
 }
