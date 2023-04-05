03207e18044aaa8e3e846bba3883074b9cbe9255
0zzysome <oscar.rosin@hotmail.com>
2023-04-05 13:25:29 +0200

    almost done scoreboard

---
+++ SlutProject/Score.cs
@@ -0,0 +1,8 @@
+using System;
+
+
+public class Score
+{
+    public int score { get; set; }
+    public String Name { get; set; }
+}
--- SlutProject/Scoreboard.cs
+++ SlutProject/Scoreboard.cs
@@ -1,15 +1,29 @@
 using System;
 
-
-
 public class Scoreboard
 {
-    
-    public Dictionary<string, int> ScoreList {get; set;}
 
+    public List<Score> ScoreList { get; set; } = new();
 
-    public void AddScoreToBoard()
+    private string fName;
+
+    public void SortScoreBoard()
     {
-        //
+        ScoreList = ScoreList.OrderBy(s => s.score).ToList();
     }
+
+    public Scoreboard(string filename)
+    {
+        fName = filename;
+        //läser filen och byter listan till scorelist
+        string i = File.ReadAllText(filename);
+        ScoreList = JsonSerializer.Deserialize<List<Score>>(i);
+    }
+
+    public void Save()
+    {
+        string json = JsonSerializer.Serialize<List<Score>>(ScoreList);
+        File.WriteAllText("scores.json", json);
+    }
+    
 }
